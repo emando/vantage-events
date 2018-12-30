442de98a748b63d2edba9e6d9730a378e644f754
Johan Stokking <johan@stokking.nl>
2018-12-30 17:42:27 +0100

    Add logging and NATS configuration to root command

--- cmd/aggregator/cmd/root.go
+++ cmd/aggregator/cmd/root.go
@@ -4,18 +4,38 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
-var cfgFile string
+var (
+	cfgFile string
+	debug   bool
+	logger  *zap.Logger
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "aggregator",
 	Short: "Event Aggregator for Vantage event streaming.",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		var err error
+		if viper.GetBool("debug") {
+			logger, err = zap.NewDevelopment()
+		} else {
+			logger, err = zap.NewProduction()
+		}
+		if err != nil {
+			log.Fatalf("failed to initialize zap (%v)", err)
+		}
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		logger.Sync()
+	},
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -29,21 +49,32 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.aggregator.yaml)")
+	rootCmd.PersistentFlags().BoolP("debug", "d", false, "enable debugging")
+	rootCmd.PersistentFlags().String("driver", "nats", "driver (nats)")
+
+	rootCmd.PersistentFlags().String("nats-url", "nats://events.emandovantage.com:4222", "NATS Streaming Server URL")
+	rootCmd.PersistentFlags().String("nats-user", "", "NATS username")
+	rootCmd.PersistentFlags().String("nats-password", "", "NATS password")
+	rootCmd.PersistentFlags().Bool("nats-tls", true, "use TLS for NATS")
+	rootCmd.PersistentFlags().String("nats-cluster-id", "vantage", "NATS cluster ID")
+	rootCmd.PersistentFlags().String("nats-client-id", "aggregator", "NATS client ID")
+
+	viper.BindPFlags(rootCmd.PersistentFlags())
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	}
-
 	viper.SetConfigName(".aggregator")
 	viper.AddConfigPath("$HOME")
 	viper.SetEnvPrefix("vantage")
 	viper.AutomaticEnv()
 
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	}
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+		fmt.Println("using config file:", viper.ConfigFileUsed())
 	}
 }
